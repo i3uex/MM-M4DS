11ba1b74e9f1e4ca2cab19a4ddf79a25bdc08aa4
CCamberoR <157109629+CCamberoR@users.noreply.github.com>
2024-03-19 12:52:45 +0100

    STARTED calls to functions (Preconditions)

--- Acceleo.MM_M4DS_main/Generated/Model data set with metanode (KNIME).py
+++ Acceleo.MM_M4DS_main/Generated/Model data set with metanode (KNIME).py
@@ -1,7 +1,3 @@
 from DataProcessing import DataProcessing
 #Clase main (por ahora)
 print('Model data set with metanode (KNIME) tested')
-print()
-print()
-dataProcessing=DataProcessing()
-dataProcessing.generateDataProcessing()
--- Acceleo.MM_M4DS_main/src/Acceleo/MM_M4DS_Workflow/main/Generate.java
+++ Acceleo.MM_M4DS_main/src/Acceleo/MM_M4DS_Workflow/main/Generate.java
@@ -42,7 +42,7 @@ public class Generate extends AbstractAcceleoGenerator {
      *
      * @generated
      */
-    public static final String[] TEMPLATE_NAMES = { "generateElement" };
+    public static final String[] TEMPLATE_NAMES = { "generateDataProcessing" };
     
     /**
      * The list of properties files from the launch parameters (Launch configuration).
