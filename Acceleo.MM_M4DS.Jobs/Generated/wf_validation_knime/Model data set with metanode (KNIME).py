c87616d349f678bfe91b813a9ee4863427435e3f
Carlos Breuer Carrasco <157109555+carlosbc24@users.noreply.github.com>
2024-08-30 01:48:18 +0200

    MERGE Acceleo.MM_M4DS.Jobs Acceleo Project from dev_ccr branch

---
+++ Acceleo.MM_M4DS.Jobs/Generated/wf_validation_knime/Model data set with metanode (KNIME).py
@@ -0,0 +1,227 @@
+print('----------------------INPUT DATA DICTIONARIES----------------------')
+dataDictionary='./knime_dataDictionaries/missing_input_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByDerivedValue_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByDerivedValue_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_input_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByFixValue_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByFixValue_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_input_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByNumericOp_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByNumericOp_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_input_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByNumericOp_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByNumericOp_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/missing_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+rowFilter_input_DataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/rowFilter_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+rowFilterRange_output_DataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/rowFilter_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+columnFilter_input_DataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/columnFilter_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+columnFilter_output_DataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/columnFilter_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+mapping_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/ruleEngine_territory_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+mapping_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/ruleEngine_territory_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+mapping_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='invalid'
+
+print('---------------------------------------')
+
+invalid=pd.read_csv(dataDictionary, sep='invalid')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/ruleEngine_instate_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+categoricalToContinuous_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='invalid'
+
+print('---------------------------------------')
+
+invalid=pd.read_csv(dataDictionary, sep='invalid')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/stringToNumber_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByNumericOp_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericOutliers_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+imputeByNumericOp_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericOutliers_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+binner_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericBinner_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+binner_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericOutliers_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+binner_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericBinner_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+binner_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericOutliers_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+binner_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericBinner_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+binner_output_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+dataDictionary='./knime_dataDictionaries/numericOutliers_output_dataDictionary.csv'
+
+print('---------------------------------------')
+
+binner_input_dataDictionary=pd.read_csv(dataDictionary, sep=',')
+
+print('_______________________________________')
+
+print('----------------------OUTPUT DATA DICTIONARIES----------------------')
+print('job_binner(avg_income)_output_dataDictionary')
+
+print('---------------------------------------')
+
+print('binner(avg_income)_output_dataDictionary')
+print('./knime_dataDictionaries/numericBinner_output_dataDictionary.csv')
+
+print('_______________________________________')
+
