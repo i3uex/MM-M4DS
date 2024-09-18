b7fbc6c7aa8f166a3eb1cdff068a75728f5f78f9
CCamberoR <157109629+CCamberoR@users.noreply.github.com>
2024-09-18 09:23:19 +0200

    STARTED environment code generation

---
+++ Acceleo.MM_M4DS/Generated/environment/deploy_docker_app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+sudo apt-get update --yes
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update --yes
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin --yes
+
+docker build -t docker_app -f Dockerfile .
+
+clear
+
+docker run -it docker_app
+
+
