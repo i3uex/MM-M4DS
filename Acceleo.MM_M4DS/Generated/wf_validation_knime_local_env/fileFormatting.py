0a1daa2e320f59968e601f6b490dd8adcfd44a88
CCamberoR <157109629+CCamberoR@users.noreply.github.com>
2024-10-17 12:55:53 +0200

    ADDED database libraries imports

--- Acceleo.MM_M4DS/Generated/wf_validation_knime_local_env/fileFormatting.py
+++ Acceleo.MM_M4DS/Generated/wf_validation_knime_local_env/fileFormatting.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import h5py
 import pyarrow
+from sqlalchemy import create_engine
 
 missing_input_dataDictionary=pd.read_csv('/wf_validation_python/data/missing_input_dataDictionary.csv', sep = ',')
 missing_input_dataDictionary.to_parquet('/wf_validation_python/data/missing_input_dataDictionary.parquet')
