b10f4309e7d36daeabe2afecf13c046a54367271
CCamberoR <157109629+CCamberoR@users.noreply.github.com>
2024-10-21 10:59:41 +0200

    FIXED file type reading and file extensions management

--- Acceleo.MM_M4DS/Generated/environment/docker/fileFormatting.py
+++ Acceleo.MM_M4DS/Generated/environment/docker/fileFormatting.py
@@ -5,10 +5,10 @@ import pyarrow
 from sqlalchemy import create_engine
 import mysql.connector
 
-missing_output_dataDictionary=pd.read_csv('/wf_validation_python/data/missing_output_dataDictionary.csv', sep = ',')
-missing_output_dataDictionary.to_parquet('/wf_validation_python/data/missing_output_dataDictionary.parquet')
+missing_input_dataDictionary=pd.read_csv('/wf_validation_python/data/missing_input_dataDictionary.csv', sep = ',')
+missing_input_dataDictionary.to_parquet('/wf_validation_python/data/missing_input_dataDictionary.parquet')
 
 engine = create_engine('mysql+mysqlconnector://root:password@localhost/test_storage')
-test_storage_missing_input_dataDictionary = pd.read_sql('SELECT * FROM missing_input_dataDictionary;', engine)
-test_storage_missing_input_dataDictionary.to_parquet('/wf_validation_python/data/missing_input_dataDictionary.parquet')
+test_storage_missing_output_dataDictionary = pd.read_sql('SELECT * FROM missing_output_dataDictionary;', engine)
+test_storage_missing_output_dataDictionary.to_parquet('/wf_validation_python/data/missing_output_dataDictionary.parquet')
 
