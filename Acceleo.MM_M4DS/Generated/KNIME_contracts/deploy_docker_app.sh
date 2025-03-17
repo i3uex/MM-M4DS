773ebddf95f976adb14624a4557dcd3978e823a8
Carlos Cambero Rojas <157109629+CCamberoR@users.noreply.github.com>
2025-03-17 19:08:15 +0100

    Added math operation transformation and mapping transformation with substring

---
+++ Acceleo.MM_M4DS/Generated/KNIME_contracts/deploy_docker_app.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "data" ]; then
+    mkdir -p data
+fi
+
+sudo apt-get update --yes
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update --yes
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin --yes
+
+docker build --no-cache -t mint:latest -f Dockerfile .
+
+clear
+
+cp /home/carlos/datasets/missing_input_dataDictionary.csv "$(pwd)/data/"
+
+
+
+
+docker run -it --rm --name docker-contracts --network host --mount type=bind,source="$(pwd)/data",target=/wf_validation_contracts/data mint:latest
+
+docker rmi mint:latest
+
+clear
+
+echo -e "Exiting the application...\n"
