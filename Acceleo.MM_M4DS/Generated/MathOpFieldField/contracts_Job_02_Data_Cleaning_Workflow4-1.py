6131f55d78b702941dd05fad8d29fb7b725473b9
Carlos Cambero Rojas <157109629+CCamberoR@users.noreply.github.com>
2025-03-24 09:30:33 +0100

    Updated code generation

---
+++ Acceleo.MM_M4DS/Generated/MathOpFieldField/contracts_Job_02_Data_Cleaning_Workflow4-1.py
@@ -0,0 +1,48 @@
+import os
+
+import pandas as pd
+import numpy as np
+import functions.contract_invariants as contract_invariants
+import functions.contract_pre_post as contract_pre_post
+from helpers.enumerations import Belong, Operator, Operation, SpecialType, DataType, DerivedType, Closure, FilterType, MapOperation, MathOperator
+from helpers.logger import set_logger
+import pyarrow
+from functions.PMML import PMMLModel
+
+def generateWorkflow():
+	#-----------------New DataProcessing-----------------
+	mathOperationFieldField_input_DataDictionary=pd.read_parquet('ruta/data/output/CSV_Reader_output_dataDictionary.parquet')
+
+	if os.path.exists('ruta/data/output/Math_Formula_output_dataDictionary.parquet'):
+		mathOperationFieldField_output_DataDictionary_df=pd.read_parquet('ruta/data/output/Math_Formula_output_dataDictionary.parquet')
+
+	missing_values_mathOperationFieldField_PRE_valueRange=[]
+	if contract_pre_post.check_missing_range(belong_op=Belong(0), data_dictionary=mathOperationFieldField_Difference_in_Latitude/Altitude__input_dataDictionary_df, field='Latitude', 
+									missing_values=missing_values_mathOperationFieldField_PRE_valueRange,
+									quant_abs=None, quant_rel=None, quant_op=None):
+		print('PRECONDITION mathOperationFieldField(Difference in Latitude/Altitude)_PRE_valueRange VALIDATED')
+	else:
+		print('PRECONDITION mathOperationFieldField(Difference in Latitude/Altitude)_PRE_valueRange NOT VALIDATED')
+	
+	missing_values_mathOperationFieldField_POST_valueRange=[]
+	if contract_pre_post.check_missing_range(belong_op=Belong(1), data_dictionary=mathOperationFieldField_Difference_in_Latitude/Altitude__output_dataDictionary_df, field='Difference in Latitude/Altitude', 
+									missing_values=missing_values_mathOperationFieldField_POST_valueRange,
+									quant_abs=None, quant_rel=None, quant_op=None):
+		print('POSTCONDITION mathOperationFieldField(Difference in Latitude/Altitude)_POST_valueRange VALIDATED')
+	else:
+		print('POSTCONDITION mathOperationFieldField(Difference in Latitude/Altitude)_POST_valueRange NOT VALIDATED')
+	
+	if contract_invariants.check_inv_math_operation(data_dictionary_in=mathOperationFieldField_Difference_in_Latitude/Altitude__input_dataDictionary_df,
+											data_dictionary_out=mathOperationFieldField_Difference_in_Latitude/Altitude__output_dataDictionary_df,
+											math_op=MathOperator(1),
+											firstOperand='Latitude', isFieldFirst=True, secondOperand='Altitude', isFieldSecond=True
+											belong_op_out=Belong(0), field_in='Difference in Latitude/Altitude', field_out='Difference in Latitude/Altitude'):
+		print('INVARIANT mathOperationFieldField(Difference in Latitude/Altitude)_INV_condition VALIDATED')
+	else:
+		print('INVARIANT mathOperationFieldField(Difference in Latitude/Altitude)_INV_condition NOT VALIDATED')
+	
+	
+	
+	
+set_logger("contracts")
+generateWorkflow()
