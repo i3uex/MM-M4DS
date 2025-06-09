8c2f87f721b3a8b2ee27a64d24314e4637fd18c8
Carlos Breuer Carrasco <carlos.breuer@hotmail.com>
2025-06-09 14:09:47 +0200

    Delete Generated directory

--- Generated/MathOpFieldFixValue/fileFormatting.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import json
-import h5py
-import pyarrow
-				
-mapping_native-country__input_dataDictionary=pd.read_csv('esta/data/output/Rule_Engine_output_dataDictionary.csv', sep = ',')
-mapping_native-country__input_dataDictionary.to_parquet('esta/data/output/Rule_Engine_output_dataDictionary.parquet')
